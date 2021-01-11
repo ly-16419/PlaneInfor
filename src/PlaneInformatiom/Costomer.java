e6c7f8ed0d4ea83812258ec323ea0a8648b3a47a
Administrator <1641911975@qq.com>
2021-01-11 16:02:31 +0800

    第二次提交：Plane航班信息 Order订单

--- src/PlaneInformatiom/Costomer.java
+++ src/PlaneInformatiom/Costomer.java
@@ -1,11 +1,31 @@
 package PlaneInformatiom;
 
+import java.util.Date;
+
 public class Costomer {
     private String id;
     private String name;
     private String phone;
-    private String cardid;
+    private String cardid;//身份证号码
+    private Date birthDate;//出生日期
+    private String customer;//客户类型
 
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
     public String getId() {
         return id;
     }
